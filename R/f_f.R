c1ad149be7fc71e88df7fb6f3755d433c1d93e28
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2019-05-17 11:36:45 +0200

    add function for density of recovered individuals with documentation, changed name for subdensity function

--- R/f_f.R
+++ R/f_f.R
@@ -1,6 +1,6 @@
-#' density function for recovered individuals
+#' density function for recovered individuals only
 #'
-#' This function sets up the density function for every spatiotemporal point for recovered individuals
+#' This function sets up a valid density function for every spatiotemporal point for recovered individuals
 #' by means of given survival, migratory connectivity and recovery probability.
 #' @param w decimal number: spatial point of recovery (1-dimensional)
 #' @param t integer: temporal component of recovery
@@ -10,9 +10,17 @@
 #' @param r constant: recovery probability, has to be constant over whole wintering area
 #' and independent of breeding area
 #' @param T integer: length of observation period
+#' @param f_f function: subdensity of recovered individuals
+#' @param f_nf function: subdensity of not recovered individuals
+#' @param lb decimal: lower bound of wintering area
+#' @param ub decimal: upper bound of wintering area
 #' @return density of recovered individuals for the specified parameters
 #' @export
 #' @examples f_f()
-f_f <- function(w,t,b,s,m,r,T){
-  f(b,w)*(s(w)^(t-1)*(1-s(w))*r)
+# PhD Saskia Schirmer
+# 15.05.2019
+# continuous model
+# density function for recovered individuals only (normalized subdensity)
+f_f <- function(w,t,b,lb,ub,s,m,r,T,f_f_sub, f_nf_sub){
+  f_f_sub(w,t,b,s,m,r,T)/(1-integrate(f_nf_sub,lb,ub, b = b,s= s, m=f, r=r, T = T)$value)
 }
