b5d44bad3460ad6145b27f0c35d427298d590289
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2019-06-04 12:01:31 +0200

    add plotS function

---
+++ R/plotS.R
@@ -0,0 +1,32 @@
+#' plot true and estimated survival function
+#'
+#' This function estimates the survival from a kernel density estimate of the data
+#' of recovered individuals. It uses the data of all breeding areas at once.
+#' @param res_x resolution in space
+#' @param markRecaptureObject object of class markRecaptureObject
+#' (see markRecaptureObject())
+#' @return vector of length res_x with survival probabilities dependent on space
+#' @export
+#' @examples plotS()
+
+
+plotS <- function(res_x,markRecaptureObject,s_fit,pdf = FALSE){
+  s <- markRecaptureObject$winteringArea$survival
+
+  if(pdf) pdf("estimateS.pdf", width = 10,height=10)
+
+  par(mar = c(5,4,4,15)+0.1, mfrow = c(1,1))
+  plot(1:res_x,s(seq(0,1,length.out = res_x)), col = "grey50", ylim = c(0,1), type="l",lwd = 2,
+       xlab = "wintering area", ylab = "survival probability",
+       xaxt = "none", lty = 2)
+  axis(1, seq(0,res_x,length.out = res_x/20),
+       seq(0,1,length.out = res_x/20))
+
+  lines(1:res_x,s_fit, col = "red")
+
+  legend(110,0.8,col = c("grey50","red"),
+         legend = c( "true s", "estimated s"),
+         xpd = TRUE, lty = c(3,1))
+  par(mar = c(5,4,4,10)+0.1)
+  if(pdf) dev.off()
+}
