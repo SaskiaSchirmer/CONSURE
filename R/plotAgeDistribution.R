a80bd2ed7d03d90ecf9cfb22b8acc6aea0d2ace5
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2019-07-11 15:12:44 +0200

    plot age distribution

---
+++ R/plotAgeDistribution.R
@@ -0,0 +1,19 @@
+#' plot age distribution of raw recoveries of real world data
+#'
+#' This function plots the age distribution of recoveries in the real world data as a histogram.
+#' @param markRecaptureObject object of class markRecaptureObject
+#' (see markRecaptureObject())
+#' @param pdf logical, saves image as pdf-file if TRUE. Defaults to FALSE.
+#' @param pdfName string to name pdf-file, please include ".pdf"!
+#' @return histogram of age of raw recoveries, depending on arguments plot as pdf or to plot to device
+#' @export
+#' @examples plotAgeDistribution()
+
+plotAgeDistribution <- function(markRecaptureObject, pdf = FALSE, pdfName = "ageDistribution.pdf",
+                              areaNames = NULL, freq = TRUE){
+  if(pdf) pdf(pdfName)
+    age <- markRecaptureObject$winteringArea$data$age
+    T <- markRecaptureObject$observationTime
+    hist(age, breaks = 0:T + 0.5, freq = freq)
+  if(pdf) dev.off()
+}
