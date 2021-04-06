e330622e15676e7b5208ad0fd4ac3a68f4f7a1f0
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2021-04-06 09:33:36 +0200

    add smoothing constrained of optimization problem

---
+++ R/Lh.R
@@ -0,0 +1,23 @@
+#' function defining the smoothness of a bspline
+#'
+#' This function defines the smoothness of a bspline as the second derivative
+#' @param beta vector of parameters of the b-spline
+#' @param dim dimension of space
+#' @param A matrix of second derivative of b-spline
+#' @param res spatial resolution of the estimates
+#' @param inside specifies if a cell of the gridded window is inside the window of the data
+#'               or not. Vector of logicals.
+#'
+#' @return function defining the distance between a b-spline and
+#'         discrete migratory connectivity depending on the parameters
+#' @export
+#' @examples Lh(w,beta,knots,degree)
+
+Lh <- function(beta, dim,A,res,normalize){
+
+    func <- function(beta){
+        t(beta)%*%A%*%beta/normalize
+    }
+
+    return(func)
+}
