de1e4467a7612f5d3a3f1236be7a1d5d558da36c
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2021-04-09 16:51:39 +0200

    correct documentation

--- R/robinsInit.R
+++ R/robinsInit.R
@@ -1,7 +1,9 @@
 #' raw real-world data for robins
 #'
-#' A dataset containing data simulated with constant recovery probability in
-#' one-dimensional space and continuous estimates of all parameters.
+#' A dataset containing the raw data of robins marked at the ringing station
+#' Greifswalder Oie, Germany, during autumn migration and dead recoveries between
+#' November and February in Europe and Africa.
+#'
 #'
 #' @docType data
 #' @usage data(robinsInit)
@@ -12,8 +14,8 @@
 #'   \item{window}{object of class owin containing the boundaries of the world map in a rectangle around the data points}
 #'   \item{observationTime}{numeric, length of observation time. One length for all individuals in the dataset.}
 #' }
-#' 
-#' @references This data set was artificially created for the CONSURE package.
+#'
+#' @references The data is provided by the Verein Jordsand e.V. and the Beringungszentrale Hiddensee.
 #' @keywords datasets
 #' @examples
 #'
