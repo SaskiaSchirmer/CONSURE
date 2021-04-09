497c05fb91c1594b97fe8c8c6b21fddc0a7f945b
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2021-04-09 10:00:04 +0200

    write function to name real-world data with default column names

---
+++ R/renameData.R
@@ -0,0 +1,34 @@
+#' rename recovery data with default column names
+#'
+#' This function renames the recovery data with default column names.
+#' @param recoveryData data.frame with dead recoveries, containing information on breeding
+#'  origin, geographical coordintes of recovery and age at death
+#' @param xname character, name of column containing information on longitude,
+#' specify this in one-dimensional case
+#' @param yname character, name of column containing information on latitude,
+#' defaults to NULL, do not specify in one-dimensional case, but use xname instead
+#' @param timename character, name of column containing information on age at death
+#' @param markAreaName character, name of column containing information on breeding origin
+#' @return recoveryData with default column names
+#' @export
+#' @examples renameData()
+#'
+renameData <- function(recoveryData, xname, yname = NULL, timename, markAreaName){
+
+  if(is.null(yname)){
+    colnamesMatrix <- matrix(c(markAreaName,xname,timename,
+                       "markArea", "longitude","age"),
+                     ncol = 2, nrow = 3)
+  } else{
+    colnamesMatrix <- matrix(c(markAreaName,xname,yname,timename,
+                       "markArea","longitude","latitude","age"),
+                     ncol = 2, nrow = 4)
+  }
+
+  colnames(colnamesMatrix) <- c("real","default")
+
+  index <- match(colnamesMatrix[,"real"], colnames(recoveryData))
+
+  colnames(recoveryData) <- colnamesMatrix[,"default"][index]
+  return(recoveryData)
+}
