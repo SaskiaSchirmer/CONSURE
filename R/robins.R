b634400a2e0c8cfdbcfe845246885919575af998
Saskia Schirmer <saskia.schirmer@uni-greifswald.de>
2021-04-09 16:54:38 +0200

    added example markRecaptureObject for real-world data

---
+++ R/robins.R
@@ -0,0 +1,23 @@
+#' example for real-world data: robins
+#'
+#' A dataset containing the raw data of robins marked at the ringing station
+#' Greifswalder Oie, Germany, during autumn migration and dead recoveries between
+#' November and February in Europe and Africa. Additionally, kernel
+#' density, survival, migratory connectivity and recovery probability are estimated
+#' by the continuous approach and stored in the markRecaptureObject.
+#'
+#' @docType data
+#' @usage data(robins)
+#' @format A markRecaptureObject.
+#' \describe{
+#'   \item{robins}{markRecaptureObject}
+#' }
+#'
+#' @references The data is provided by the Verein Jordsand e.V. and the Beringungszentrale Hiddensee.
+#' @keywords datasets
+#' @examples
+#'
+#' data(robins)
+#' str(robins)
+#'
+"robins"
